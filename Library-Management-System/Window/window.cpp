ca2d193d67d2710ec453281bc34c9b1fa49302be
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-19 20:18:12 +0500

    Create window.cpp

---
+++ Library-Management-System/Window/window.cpp
@@ -0,0 +1,54 @@
+#include "window.h"
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include "../Signup Login/Signup.h"
+#include "../Signup Login/authentication.h"
+
+
+
+void Window::initVariables() {
+
+	this->window = nullptr;
+
+	sf::VideoMode desktopScreen = sf::VideoMode::getDesktopMode();
+
+	this->videoMode.height = desktopScreen.height;
+	this->videoMode.width = desktopScreen.width;	
+}
+
+void Window::initWindow() {
+
+	this->window = new sf::RenderWindow(this->videoMode, "Library Mangement System", sf::Style::Close);
+}
+
+Window::Window() {
+
+	initVariables();
+	initWindow();
+}
+
+Window::~Window() {
+
+	delete this->window;
+}
+
+void Window::windowLoop() {
+
+	while (this->window->isOpen()) {
+
+		while (this->window->pollEvent(evnt)) {
+
+			switch (evnt.type)
+			{
+			case sf::Event::Closed:
+
+				this->window->close();
+				break;
+			}
+
+			Authentication auth(this->window, this->evnt);
+			auth.authenticationOptions();
+		}
+	}
+}
