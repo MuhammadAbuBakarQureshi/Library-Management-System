a7fcbb6b05786bc91c102f451612db29414466c4
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-21 16:02:56 +0500

    Update Main-Menu.cpp

--- Library-Management-System/Main Menu/Main-Menu.cpp
+++ Library-Management-System/Main Menu/Main-Menu.cpp
@@ -2,16 +2,34 @@
 #include <string>
 
 #include "../Login-Registration/Login-Registration/Login-Registration.h"
+#include "../User Main Menu/User-Main-Menu.h"
 
 using namespace std;
 
 void mainMenu() {
 
+	UserMainMenu userMainMenu;
+	
+	userMainMenu.run();
 
-	bool isAuthenticate = loginRegistration();
 
-	if (isAuthenticate) {
+	// return true if successful
 
-		cout << "Done" << endl;
-	}
+	//bool isAuthenticate = loginRegistration();
+
+	//BookHashTableByID* bookHashTableByID = new BookHashTableByID();
+
+	//bookHashTableByID->read_file();
+
+	//string bookID, bookTitle, bookAuthor, summary, genre;
+
+	//bookTitle = "Atomic habits";
+	//bookAuthor = "Bakar";
+	//summary = "This books helps you to replace your bad habits with good ones";
+	//genre = "Self-Improvement";
+
+	//bookHashTableByID->addBook(bookTitle, bookAuthor, summary, genre);
+
+	//bookHashTableByID->write_file();
+	
 }
