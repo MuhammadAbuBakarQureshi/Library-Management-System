99f431c598f211b1f51925ecdd58b0929d21e146
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-21 20:46:16 +0500

    Update Main-Menu.cpp

--- Library-Management-System/Main Menu/Main-Menu.cpp
+++ Library-Management-System/Main Menu/Main-Menu.cpp
@@ -4,13 +4,42 @@
 #include "../Login-Registration/Login-Registration/Login-Registration.h"
 #include "../User Main Menu/User-Main-Menu.h"
 
+// FOR testing
+
+#include "../Books/Books.h"
+
 using namespace std;
 
 void mainMenu() {
 
+	/*BookHashTableByID* bookHashTableByID = new BookHashTableByID();
+
+	bookHashTableByID->restoreBooks();
+
+	string bookID, bookTitle, bookAuthor, summary, genre;
+
+	bookTitle = "Atomic habits";
+	bookAuthor = "Bakar";
+	summary = "This books helps you to replace your bad habits with good ones";
+	genre = "Self-Improvement";
+
+	
+	bookHashTableByID->addBook(bookTitle, bookAuthor, summary, genre);
+
+	bookTitle = "Money power";
+	bookAuthor = "Abdullah";
+	summary = "It explains power of money";
+	genre = "Power";
+
+	bookHashTableByID->addBook(bookTitle, bookAuthor, summary, genre);
+
+	bookHashTableByID->storeBooks();*/
+
+
 	UserMainMenu userMainMenu;
 	
 	userMainMenu.run();
+
 
 
 	// return true if successful
