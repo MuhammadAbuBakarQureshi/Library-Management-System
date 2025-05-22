3248f28aa7615a1e6560c2f41f1e0757a18c3f28
MuhammadAbuBakarQureshi <152699100+MuhammadAbuBakarQureshi@users.noreply.github.com>
2025-05-22 10:50:20 +0500

    Update User Main Menu.cpp

--- Library-Management-System/User Main Menu/User Main Menu.cpp
+++ Library-Management-System/User Main Menu/User Main Menu.cpp
@@ -30,17 +30,62 @@ void UserMainMenu::insertBook() {
 	cout << "\n\nEnter genre: ";
 	getline(cin >> ws, genre);
 
-	bookHashTableByID->addBook(bookTitle, bookAuthor, summary, genre);
+	bookHashTableByID->insertBook(bookTitle, bookAuthor, summary, genre);
 }
 
 void UserMainMenu::findBook() {
 
-	string bookID;
+	std::cout << "\n\n\t\tSearch By\n\n" << std::endl;
 
-	cout << "\n\nEnter BookID: ";
-	getline(cin >> ws, bookID);
+	bool isRunning = true;
 
-	bookHashTableByID->searchByID(bookID);
+	while (isRunning) {
+
+		std::cout << "\n\n1. Search by ID." << std::endl
+			<< "2. Search by Title. \n\n" << std::endl
+			<< "3. Search by Author. \n\n" << std::endl
+			<< "4. Exit\n\n" << std::endl;
+
+
+		string options;
+
+		std::cin >> options;
+
+		if (options == "1") {
+
+			string bookID;
+
+			cout << "\n\nEnter BookID: ";
+			getline(cin >> ws, bookID);
+
+			bookHashTableByID->searchByID(bookID);
+		}
+		else if (options == "2") {
+
+			string bookTitle;
+
+			cout << "Enter Book Title: ";
+			getline(cin >> ws, bookTitle);
+
+			bookHashTableByID->bookHashTableByBookTitle->searchByTitle(bookTitle);
+		}else if (options == "3") {
+		
+			string bookAuthor;
+
+			cout << "Enter Book Author: ";
+			getline(cin >> ws, bookAuthor);
+
+			bookHashTableByID->bookHashTableByBookAuthor->searchByAuthor(bookAuthor);
+		}
+		else if (options == "4") {
+
+			isRunning = false;
+		}
+		else {
+
+			cout << "Enter correct option" << endl;
+		}
+	}
 }
 
 
@@ -48,9 +93,9 @@ void UserMainMenu::run() {
 
 	std::cout << "\n\n\t\tUser Main Menu\n\n" << std::endl;
 
-	bool isClose = false;
+	bool isRunning = true;
 
-	while (!isClose) {
+	while (isRunning) {
 
 		std::cout << "\n\n1. Insert book." << std::endl
 			<< "2. Find Book \n\n" << std::endl
@@ -70,7 +115,7 @@ void UserMainMenu::run() {
 		}
 		else if (options == "3") {
 
-			isClose = true;
+			isRunning = false;
 		}
 		else {
 
